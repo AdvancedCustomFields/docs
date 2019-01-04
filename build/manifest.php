4318032942033f42af329cc6b6ac026892cb9e23
Elliot Condon <e@elliotcondon.com>
2019-01-04 11:05:55 +1100

    Adding build manifest script

---
+++ build/manifest.php
@@ -0,0 +1,13 @@
+<?php
+
+// Vars.
+define('DOCS_PATH', dirname(__FILE__) . '/');
+
+// Include dependancies.
+include_once( ACF_DOCS_PATH . 'vendor/autoload.php');
+
+// Test
+echo '<pre>';
+	print_r('s');
+echo '</pre>';
+die;
